09c5cb3c10cf14d48898f4f4e357b2c05c05e92d
Forlaenu <93723560+Forlaenu@users.noreply.github.com>
2021-12-17 13:36:14 -0600

    added a bunch of API call functions to the script

---
+++ src/script.js
@@ -0,0 +1,200 @@
+// Cayden - figuring out the API, what to send, what is received, and how to parse
+// will probably use localStorage to keep api calls down
+
+// API KEY, key=${caydensKey}
+const apiURL = "https://api.rawg.io/api/"
+const gameAPIurl = `${apiURL}games/`
+const caydensKey = "key=ceb0a7023e6f466bacf471f8695bb3f1";
+
+// PLATFORM ID's for API: &platforms=${platformPC}
+const platforms = {
+    pc: 4,
+    console: {
+        xboxOne: 1,
+        playstation4: 18,
+        nintendoSwitch: 7,
+        playstation5: 187,
+        xboxSeriesX: 186,
+        xbox360: 14,
+        playstation3: 16,
+    },
+    mobile: {
+        iOS: 3,
+        android: 21,
+    },
+}
+
+// STORE ID's for API 
+const store = {
+    steam: 1,
+    playstationStore: 3,
+    nintendoStore: 6,
+    xbox360Store: 7,
+}
+
+// # OF ITEMS TO RETURN, &page_size=${page_size}
+const pageSizeInt = 20;
+const pageSize = `page_size=${pageSizeInt}`;
+
+// FOR SEARCHES, &${searchType[0]}=${searchBar.value} 
+// [0] being basic search, [1] being "Disable fuzziness for the search query", [2] being strict search
+const searchType = ["search", "search_precise", "search_exact"]
+
+// setting up a function to call in browser that doesn't run every page load
+// then appending results to localStorage for playing around with
+const searchGameList = searchGames("Theft")
+function searchGames(searchTerm){
+    return fetch(`${apiURL}games?${caydensKey}&${pageSize}&${searchType[0]}=${searchTerm}`)
+    .then(response => response.json())
+    .then(function(data){
+        const requests = data.results.map((game)=>{
+            return getGameData(game)
+        })
+        return Promise.all(requests)
+    })
+}
+
+// THESE MAY ONLY WORK WHEN USING "&search= " API parameter
+// THIS RETURNS 1 GAME OBJECT'S ATTRIBUTES, DOES NOT WORK ON A LIST
+// USE IN A LOOP OVER A LIST, CALLING getGenres() on one dictionary result ie. data.results[i] <- i being important here. data.results is a list
+
+// getGenres returns a list of the gameObject's genres, as strings. A list because there's usually more than one ie: "action", "adventure"
+function getGenres(gameObject){
+    const genreList = [];
+    for(i=0; i<gameObject.genres.length;i++){
+        genreList.push(gameObject.genres[i].name)
+    }
+    return genreList;
+}
+
+// getPlatform returns a list of gameObject's release platforms, as strings. ie: "xboxOne", "PC" 
+// [{
+//     "platform": {
+//     "id": 4,
+//     "name": "PC",
+//     "slug": "pc"
+//     }
+// },]
+function getPlatform(gameObject){
+    const platformList = [];
+    for(i=0; i<gameObject.platforms.length;i++){
+        platformList.push(gameObject.platforms[i].platform.name)
+    }
+    return platformList;
+}
+
+// getBackgroundIMG returns a string URL of the designated "background image" for the gameObject
+function getBackgroundIMG(gameObject){
+    return gameObject.background_image;
+}
+
+// getRatings will return the RAWG version of gameObject's user rating... haven't decided how just yet
+function getRatings(gameObject){
+    // "ratings": [
+    //     {
+    //     "id": 5,
+    //     "title": "exceptional",
+    //     "count": 847,
+    //     "percent": 59.99
+    //     },
+    //     {
+    //     "id": 4,
+    //     "title": "recommended",
+    //     "count": 361,
+    //     "percent": 25.57
+    //     },
+    //     {
+    //     "id": 3,
+    //     "title": "meh",
+    //     "count": 140,
+    //     "percent": 9.92
+    //     },
+    //     {
+    //     "id": 1,
+    //     "title": "skip",
+    //     "count": 64,
+    //     "percent": 4.53
+    //     }
+    //     ],
+}
+
+// getReleasedDate returns a string of the gameObject's release date in year-month-day notation
+function getReleasedDate(gameObject){
+    return gameObject.released;
+}
+
+// getName returns the string of the object's display name, already capitalized 
+function getName(gameObject){
+    return gameObject.name;
+}
+
+// getMetaScore returns the integer metascore rating 
+function getMetaScore(gameObject){
+    return gameObject.metacritic;
+}
+
+// getESRB returns the string ESRB rating. Want to spice it up with the age recommendation that's not provided via just gameObject.esrb_rating
+// {
+//     "id": 4,
+//     "name": "Mature",
+//     "slug": "mature",
+//     "name_en": "Mature",
+//     "name_ru": "С 17 лет"
+// },
+function getESRB(gameObject){
+    // SWITCH CASE maybe?
+    // switch(gameObject.esrb_rating)
+    // case("Mature")
+    // return "17+ Mature"
+    // etc
+    return gameObject.esrb_rating.name;
+}
+// getGameID will grab RAWG's unique id for the game that will be used to grab more particular data 
+function getGameID(gameObject){
+    return gameObject.id;
+}
+
+// getScreenshots will return a list of the screenshots posted on RAWG
+// [{
+//     "id": -1,
+//     "image": "https://media.rawg.io/media/games/2ad/2ad87a4a69b1104f02435c14c5196095.jpg"
+// },]
+// from what i can tell, [0] always has an id of -1 (ive totally used this before as a way to get a "default" value, in this case, a background image)
+function getScreenshots(gameObject){
+    const screenshotList = [];
+    for(i=0;i<gameObject.short_screenshots.length;i++){
+        if(gameObject.short_screenshots[i].id != -1){
+            screenshotList.push(gameObject.short_screenshots[i].image)
+        }
+    }
+    return screenshotList;
+}
+const newData = [];
+// getGameDescription first gets ID from original list, then sends an API call for game specific details. Returns HTML ie <p>Text Text Text</p>
+function getGameData(gameObject){
+    let gameID = getGameID(gameObject);
+    // const newDiv = document.createElement("div")
+    return fetch(`${gameAPIurl}${gameID}?${caydensKey}`)
+    .then(res => res.json())
+    .then(function(data){
+        // gameDescription = data.description;
+        // newDiv.innerHTML = gameDescription;
+        // document.body.appendChild(newDiv)
+        return Object.assign({},gameObject,data);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
